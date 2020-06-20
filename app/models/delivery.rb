e1417dc82ddd3a8a15f4e7c3b0f676db0dc36800
vagrant <vagrant@localhost.localdomain>
2020-06-20 10:25:14 +0000

    注文機能実装中

--- app/models/delivery.rb
+++ app/models/delivery.rb
@@ -1,2 +1,8 @@
 class Delivery < ApplicationRecord
+	belongs_to :customer
+
+	def customers_information
+		self.postcode + self.address
+	end
+
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,8 @@
 class Order < ApplicationRecord
+	belongs_to :customer
+	has_many :order_items
+
+	def self.user_deliveries
+	  self.postcode + self.address + self.first_name
+    end
 end
