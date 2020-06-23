b605610d4ef91069efe1d3be2b0f6a0f7b3ee5a6
vagrant <vagrant@localhost.localdomain>
2020-06-23 03:48:35 +0000

    一回目テスト

--- app/controllers/admins/items_controller.rb
+++ app/controllers/admins/items_controller.rb
@@ -18,7 +18,7 @@ before_action :set_genres
   def create
     @item = Item.new(item_params)
     @item.save
-    redirect_to request.referer
+    redirect_to admins_item_path(@item)
   end
 
   def edit
--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'dea0d4b6f24ed9699cefa2b117b40e4c091a10a1e7f2bccc3136d3f933f0fd6ff0c77216de0af8a090ec4d6d0457a33558be20ca1317bba04d963da27c5bd85c'
